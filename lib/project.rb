6d0924b4d322ec848db1af921d9286e07339e66c
Daniel Matney <danieljmatney@gmail.com>
2019-10-14 00:33:35 +0000

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,19 @@
+class Project
+  attr_reader :backers, :title
+  @@all = []
+  
+  def initialize(name)
+    @title = name
+    @backers = []
+  end
+  
+  def add_backer(backer)
+    @backers << backer
+    if backer.backed_projects.include?(self)
+    else backer.back_project(self)
+    end
+  end
+  
+  
+  
+end
