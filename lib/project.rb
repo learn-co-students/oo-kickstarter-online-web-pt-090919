7b533022865b4092bb5944732468d9f1f7817572
Alina Kornilova <alinakornilova78@gmail.com>
2019-10-14 21:59:13 +0000

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,13 @@
+lass Project
+  attr_accessor :title, :backers
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+  
+  def add_backer(backer)
+    @backers << backer
+    backer.backed_projects << self
+  end
+end
